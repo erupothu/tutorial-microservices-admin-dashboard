9f199b653648acb17a473da641bc53485e9c4df9
Erupothu Harish <erupothu@users.noreply.github.com>
2020-07-03 17:49:36 +0530

    Add files via upload
    admin server

---
+++ admin-server/src/main/java/com/tutorial/adminserver/AdminServerApp.java
@@ -0,0 +1,17 @@
+package com.tutorial.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
+public class AdminServerApp {
+	public static void main(String args[]) {
+		SpringApplication.run(AdminServerApp.class, args);
+	}
+}
